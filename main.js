62114f9cf23f489d7c470f0fdb344d8eed91386d
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-01-18 19:17:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+canvas =document.getElementById("myQavas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "green"
+ctx.lineWidth=5;
+ctx.arc(100,150,40,0,2*Math.PI)
+ctx.stroke()
